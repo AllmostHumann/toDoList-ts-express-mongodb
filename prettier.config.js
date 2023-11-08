b61dc7f78db9c48131a6cffeb98401a315a5091f
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-11-08 21:47:45 +0100

    First Commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  parser: "typescript",
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+};
+
+export default config;
