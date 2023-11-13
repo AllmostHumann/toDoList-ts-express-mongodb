7f261bbc502fc2009d910eaf7d271509d2241f56
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-11-13 21:50:19 +0100

    task component created

---
+++ src/to-do-list/TaskPage/task.tsx
@@ -0,0 +1,30 @@
+// import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom';
+import { Container } from '../../components/Container/container';
+import { Header } from '../../components/Header/header';
+import { Section } from '../../components/Section/section';
+import useTasksStore from '../../utils/taskStore';
+
+export const TaskPage = () => {
+  const store = useTasksStore();
+  const { taskId } = useParams();
+  const tasks = store.tasks;
+  const task = store.getTaskById(taskId, tasks);
+
+  return (
+    <Container>
+      <Header title='Task details' />
+      <Section
+        title={task ? task.content : 'Task not found 😔'}
+        body={
+          task && (
+            <div className='p-[20px] bg-white leading-normal mb-[10px] border-[1px] flex flex-wrap'>
+              <strong>Done:</strong>&nbspl;{' '}
+              {task.done ? 'Yes' : 'No'}
+            </div>
+          )
+        }
+      />
+    </Container>
+  );
+};
