19f9c948d221d1bff05d68db59f09f63f5d02f06
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-11-13 21:42:14 +0100

    tailwindconfig change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        alto: '#D9D9D9',
+        teal: '#008080',
+        silverChalice: '#B1AFAF',
+        silver: "#CCCCCC",
+        japaneseLaurel: "#008000",
+        limeade: "#3AB701",
+        sunsetOrange: "#FF4747"
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      },
+      gridTemplateColumns: {
+        auto: "auto, auto",
+        1: "1fr",
+        2: "1fr, auto",
+        3: "25px 1fr 25px"
+      },
+    },
   },
   plugins: [],
 };
