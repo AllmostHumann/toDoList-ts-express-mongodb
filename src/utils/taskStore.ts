69557c468886912efa11a3deb80c2984d1439232
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-11-14 20:51:13 +0100

    taskStore change

--- src/utils/taskStore.ts
+++ src/utils/taskStore.ts
@@ -1,6 +1,11 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
-import { getTasksFromLocalStorage } from './localStorage';
+import {
+  devtools,
+  persist,
+  createJSONStorage,
+} from 'zustand/middleware';
+// import { getTasksFromLocalStorage } from './localStorage';
+import { getExampleTasks } from './getExampleTasks';
 
 interface Task {
   id: string;
@@ -12,91 +17,103 @@ interface TasksState {
   tasks: Task[];
   hideDone: boolean;
   loading: boolean;
-  allTasksDone: boolean;
-  allTasksEmpty: boolean;
+  // allTasksDone: boolean;
+  // allTasksEmpty: boolean;
 }
 
 interface TasksStore extends TasksState {
   addTask: (task: Task) => void;
   toggleHideDone: () => void;
-  toggleTaskDone: (taskId: string) => void;
-  removeTask: (taskId: string) => void;
-  setAllDone: () => void;
+  toggleTaskDone: (id: string) => void;
+  removeTask: (id: string) => void;
+  setAllTasksDone: () => void;
   setTasks: (tasks: Task[]) => void;
-  exampleTasks: () => void;
-  exampleTasksError: () => void;
-  exampleTasksSuccess: (tasks: Task[]) => void;
   areAllTasksDone: (tasks: Task[]) => boolean;
-  areAllTasksEmpty: (tasks: Task[]) => boolean;
-  getTasksFromLocalStorage: () => void;
+  areTasksListEmpty: (tasks: Task[]) => boolean;
   getTaskById: (
-    taskId: string | undefined,
+    id: string | undefined,
     tasks: Task[],
   ) => Task | undefined;
   selectTaskByQuery: (
     query: string | null,
     tasks: Task[],
   ) => Task[];
+  getExampleTasks: () => Promise<void>;
 }
 
 const useTasksStore = create<TasksStore>()(
-  devtools((set) => ({
-    tasks: [],
-    hideDone: false,
-    loading: false,
-    allTasksDone: false,
-    allTasksEmpty: false,
-    addTask: (task) =>
-      set((state) => ({ tasks: [...state.tasks, task] })),
-    toggleHideDone: () =>
-      set((state) => ({ hideDone: !state.hideDone })),
-    toggleTaskDone: (taskId) =>
-      set((state) => ({
-        tasks: state.tasks.map((task) =>
-          task.id === taskId
-            ? { ...task, done: !task.done }
-            : task,
-        ),
-      })),
-    removeTask: (taskId) =>
-      set((state) => ({
-        tasks: state.tasks.filter(
-          (task) => task.id !== taskId,
-        ),
-      })),
-    setAllDone: () =>
-      set((state) => ({
-        tasks: state.tasks.map((task) => ({
-          ...task,
-          done: true,
+  persist(
+    devtools((set) => ({
+      tasks: [],
+      hideDone: false,
+      loading: false,
+      addTask: (task) =>
+        set((state) => ({ tasks: [...state.tasks, task] })),
+      toggleHideDone: () =>
+        set((state) => ({ hideDone: !state.hideDone })),
+      toggleTaskDone: (id) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id
+              ? { ...task, done: !task.done }
+              : task,
+          ),
         })),
-      })),
-    setTasks: (tasks) => set({ tasks, loading: false }),
-    exampleTasks: () => set({ loading: true }),
-    exampleTasksError: () => set({ loading: false }),
-    exampleTasksSuccess: (tasks) =>
-      set({ tasks, loading: false }),
-    areAllTasksDone: (tasks) =>
-      tasks.every((task) => task.done),
-    areAllTasksEmpty: (tasks) =>
-      tasks.every((task) => task.content.length === 0),
-    getTaskById: (taskId, tasks) =>
-      tasks.find((task) => task.id === taskId),
-    getTasksFromLocalStorage: () => {
-      const tasks = getTasksFromLocalStorage();
-      set({ tasks });
+      setAllTasksDone: () =>
+        set((state) => {
+          const allTasksDone = state.tasks.every(
+            (task) => task.done,
+          );
+          const tasks = state.tasks.map((task) => ({
+            ...task,
+            done: allTasksDone ? false : true,
+          }));
+          return { tasks };
+        }),
+      removeTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter(
+            (task) => task.id !== id,
+          ),
+        })),
+
+      setTasks: (tasks) => set({ tasks, loading: false }),
+      getExampleTasks: async () => {
+        try {
+          set({ loading: true });
+          await new Promise((resolve) =>
+            setTimeout(resolve, 500),
+          );
+          const exampleTaks = await getExampleTasks();
+          set({ tasks: exampleTaks });
+        } catch (error) {
+          console.error('Something went wrong :(', error);
+        } finally {
+          set({ loading: false });
+        }
+      },
+      areAllTasksDone: (tasks) =>
+        tasks.every((task) => task.done),
+      areTasksListEmpty: (tasks) =>
+        tasks.every((task) => task.content.length === 0),
+      getTaskById: (id, tasks) =>
+        tasks.find((task) => task.id === id),
+      selectTaskByQuery: (query, tasks) => {
+        if (!query || query.trim() === '') {
+          return tasks;
+        }
+        return tasks.filter(({ content }) =>
+          content
+            .toUpperCase()
+            .includes(query.trim().toUpperCase()),
+        );
+      },
+    })),
+    {
+      name: 'task-storage',
+      storage: createJSONStorage(() => localStorage),
     },
-    selectTaskByQuery: (query, tasks) => {
-      if (!query || query.trim() === '') {
-        return tasks;
-      }
-      return tasks.filter(({ content }) =>
-        content
-          .toUpperCase()
-          .includes(query.trim().toUpperCase()),
-      );
-    },
-  })),
+  ),
 );
 
 export default useTasksStore;
