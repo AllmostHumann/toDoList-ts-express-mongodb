fdd403d557f7816d7a3fdd36cf78f690de080521
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-11-26 18:21:13 +0100

    max width for content change

--- apps/frontend/src/components/Container/container.tsx
+++ apps/frontend/src/components/Container/container.tsx
@@ -1,6 +1,6 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='my-0 mx-auto text-left p-[20px] max-w-[1024px] w-full leading-normal'>
+    <main className='my-0 mx-auto text-left p-[20px] max-w-3xl w-full leading-normal'>
       {children}
     </main>
   );
