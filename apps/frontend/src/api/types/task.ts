296bb75234a082a7a568ddb50976083571dfde3c
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-01 20:38:18 +0100

    Types for tasks added

---
+++ apps/frontend/src/api/types/task.ts
@@ -0,0 +1,10 @@
+export interface TaskResult {
+  _id?: string;
+  content?: string;
+  done?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+  __v?: number;
+}
+
+export type TasksResponse = TaskResult[];
