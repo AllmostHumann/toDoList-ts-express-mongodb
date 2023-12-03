61dca8a0a96a02243218aaac5a67c256dce227dd
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-03 20:14:09 +0100

    taskButtons update

--- apps/frontend/src/components/Buttons/taskButtons.tsx
+++ apps/frontend/src/components/Buttons/taskButtons.tsx
@@ -1,14 +1,22 @@
+import { useGetTasks } from '../../api/hooks/useGetTasks';
+import { useMarkAllTasksAsDone } from '../../api/hooks/useMarkAllTasksAsDone';
+import { useMarkAllTasksAsUndone } from '../../api/hooks/useMarkAllTasksAsUndone';
 import useTasksStore from '../../utils/taskStore';
 import { Button } from './button';
 
 export const TaskButtons = () => {
   const store = useTasksStore();
-  const tasks = store.tasks;
-  const areTasksListEmpty = store.areTasksListEmpty(tasks);
-  const areAllTaskDone = store.areAllTasksDone(tasks);
-  const setAllTaskDone = store.setAllTasksDone;
+  // const tasks = store.tasks;
+  // const areTasksListEmpty = store.areTasksListEmpty(tasks);
+  // const areAllTaskDone = store.areAllTasksDone(tasks);
+  // const hidden = store.hideDone;
+  const { data: task } = useGetTasks();
+  const { mutate: setAllTaskDone } = useMarkAllTasksAsDone();
+  const { mutate: setAllTaskUndone } = useMarkAllTasksAsUndone();
   const setAllTaskHide = store.toggleHideDoneTasks;
-  const hidden = store.hideDone;
+  const hidden = task?.every((task) => task.done === false);
+  const areTasksListEmpty = task?.every((task) => task.content.length === 0);
+  const areAllTaskDone = task?.every((task) => task.done);
 
   return (
     <div className='flex flex-wrap basis-auto m-[5px] bg-white dark:bg-davysGray justify-center'>
@@ -17,7 +25,13 @@ export const TaskButtons = () => {
           <Button onClick={() => setAllTaskHide()}>
             {hidden ? 'Show' : 'Hide'} done
           </Button>
-          <Button onClick={() => setAllTaskDone()}>
+          <Button
+            onClick={() => {
+              task?.map((task) =>
+                task.done === false ? setAllTaskDone() : setAllTaskUndone(),
+              );
+            }}
+          >
             Mark all as {areAllTaskDone ? 'undone' : 'done'}
           </Button>
         </>
