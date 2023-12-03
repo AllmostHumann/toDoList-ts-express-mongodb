bd2dee29f3b53483f06d28d08a679085dc998512
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-03 16:25:32 +0100

    taskList update

--- apps/frontend/src/to-do-list/TasksPage/TasksList/tasksList.tsx
+++ apps/frontend/src/to-do-list/TasksPage/TasksList/tasksList.tsx
@@ -1,43 +1,53 @@
-import { useLocation } from 'react-router-dom';
-import useTasksStore from '../../../utils/taskStore';
-import searchQueryParamName from '../../../utils/searchQueryParamName';
 import { NavLink } from 'react-router-dom';
 import { toTask } from '../../../routers';
-import classNames from 'classnames';
 import { DoneButton } from '../../../components/Buttons/doneButton';
 import { DeleteButton } from '../../../components/Buttons/deleteButton';
 import { Content } from '../../../components/Content/content';
+import searchQueryParamName from '../../../utils/searchQueryParamName';
+import classNames from 'classnames';
+import { useLocation } from 'react-router-dom';
+import { useGetTasks } from '../../../api/hooks/useGetTasks';
+import { useDeleteTask } from '../../../api/hooks/useDeleteTask';
+import { useMarkTaskAsDone } from '../../../api/hooks/useMarkTaskAsDone';
+import { useMarkTaskAsUndone } from '../../../api/hooks/useMarkTaskAsUndone';
+import useTasksStore from '../../../utils/taskStore';
 
 export const TasksList = () => {
   const store = useTasksStore();
   const location = useLocation();
   const query = new URLSearchParams(location.search).get(searchQueryParamName);
-  const task = store.tasks;
-  const tasks = store.getTaskByQuery(query, task);
+  const { data: tasks } = useGetTasks(query);
+  const { mutate: deleteTask } = useDeleteTask();
+  const { mutate: markTaskAsDone } = useMarkTaskAsDone();
+  const { mutate: markTaskAsUndone } = useMarkTaskAsUndone();
   const hideDone = store.hideDone;
-  const removeTask = store.removeTask;
-  const toggleTaskDone = store.toggleTaskDone;
 
   return (
     <ul className='p-[20px] m-auto'>
-      {tasks.map((task) => (
+      {tasks?.map((task) => (
         <li
           className={classNames({
             'border-b-[1px] border-solid border-alto p-[5px] grid grid-cols-3 items-center gap-[10px] dark:border-gray-400':
               true,
             hidden: task.done && hideDone,
           })}
-          key={task.id}
+          key={task._id}
         >
-          <DoneButton onClick={() => toggleTaskDone(task.id)}>
+          <DoneButton
+            onClick={() => {
+              task.done === false
+                ? markTaskAsDone(task._id)
+                : markTaskAsUndone(task._id);
+            }}
+          >
             <p className='m-auto translate-y-[1px] hover:translate-y-[-0.5px]'>
               {task.done ? '✔' : '✔'}
             </p>
           </DoneButton>
           <Content done={task.done}>
-            <NavLink to={toTask({ id: task.id })}>{task.content}</NavLink>
+            <NavLink to={toTask({ _id: task._id })}>{task.content}</NavLink>
           </Content>
-          <DeleteButton onClick={() => removeTask(task.id)}>
+          <DeleteButton onClick={() => deleteTask(task._id)}>
             <p className='m-auto translate-y-[1px] hover:translate-y-[-0.5px]'>
               X
             </p>
