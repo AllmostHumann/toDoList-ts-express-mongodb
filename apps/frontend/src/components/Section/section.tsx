461a9d7dd0ca301a36bfb398232b7f4129d856e6
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-07 21:24:25 +0100

    styles change

--- apps/frontend/src/components/Section/section.tsx
+++ apps/frontend/src/components/Section/section.tsx
@@ -13,10 +13,10 @@ export const Section: React.FC<SectionContent> = ({
     <div className='overflow-hidden rounded-[5px] mb-[10px] my-auto leading-normal'>
       <header
         className='mb-[2px] p-[20px] grid border-solid 
-      border-alto md:grid-cols-auto justify-between items-center 
+      border-alto justify-between items-center 
       grid-cols-1'
       >
-        <h2 className='md:m-0 md:text-[25px] text-[20px] m-[3px] bg-white dark:bg-davysGray'>
+        <h2 className='md:m-0 md:text-[25px] text-[20px] bg-white dark:bg-davysGray'>
           {title}
         </h2>
         {extraHeaderContent}
