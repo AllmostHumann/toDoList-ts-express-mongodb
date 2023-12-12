cec698e02fab26fa7fe4aac44069ba1c50279897
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-12 22:25:06 +0100

    credentials option added

--- apps/frontend/src/api/utilities/axiosInstance.ts
+++ apps/frontend/src/api/utilities/axiosInstance.ts
@@ -4,6 +4,7 @@ import { apiBaseUrl } from '../config/apiRoutes';
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
 export { axiosInstance };
