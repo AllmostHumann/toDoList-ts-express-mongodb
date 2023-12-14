23631e1df66a0d4bcc5470fbb9dc83b4f3069cc8
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-14 23:59:06 +0100

    component update for zustand/RQ coop

--- apps/frontend/src/to-do-list/TasksPage/TasksList/tasksList.tsx
+++ apps/frontend/src/to-do-list/TasksPage/TasksList/tasksList.tsx
@@ -12,63 +12,105 @@ import { useMarkTaskAsDone } from '../../../api/hooks/tasks/useMarkTaskAsDone';
 import { useMarkTaskAsUndone } from '../../../api/hooks/tasks/useMarkTaskAsUndone';
 import DeleteIcon from '../../../components/Buttons/ButtonIcons/deleteButton.svg?react';
 import useTasksStore from '../../../utils/taskStore';
-// import { useEffect } from 'react';
+import { useGetAuthenticadedUser } from '../../../api/hooks/users/useGetAuthenticadedUser';
 
 export const TasksList = () => {
-  const store = useTasksStore();
   const location = useLocation();
-  // const { setTasks } = useTasksStore();
   const query = new URLSearchParams(location.search).get(searchQueryParamName);
   const { data: tasks } = useGetTasks(query);
+  const { data: loggedUser } = useGetAuthenticadedUser();
   const { mutate: deleteTask } = useDeleteTask();
   const { mutate: markTaskAsDone } = useMarkTaskAsDone();
   const { mutate: markTaskAsUndone } = useMarkTaskAsUndone();
-  const hideDone = store.hideDone;
-
-  // useEffect(() => {
-  //   if (tasks) {
-  //     setTasks(tasks);
-  //   }
-  // }, [setTasks, tasks]);
+  const {
+    removeTask,
+    setTaskDone,
+    hidden,
+    tasks: exampleTask,
+    getTaskByQuery,
+  } = useTasksStore();
+  const exampleTasks = getTaskByQuery(query, exampleTask);
 
   return (
     <ul className='p-[20px] m-auto'>
-      {tasks?.map((task) => (
-        <li
-          className={classNames({
-            'border-b-[1px] border-solid border-alto p-[5px] grid grid-cols-3 items-center gap-[10px] dark:border-gray-400':
-              true,
-            hidden: task.done && hideDone,
-          })}
-          key={task._id}
-        >
-          <DoneButton
-            className={classNames({
-              'w-[25px] h-[25px] border-none cursor:pointer p-0  text-white':
-                true,
-              ' bg-limeade hover:bg-japaneseLaurel dark:bg-green-700 ':
-                task.done === false,
-              ' bg-orange-500 hover:bg-orange-600 dark:bg-orange-700 ':
-                task.done === true,
-            })}
-            onClick={() => {
-              task.done === false
-                ? markTaskAsDone(task._id)
-                : markTaskAsUndone(task._id);
-            }}
-          >
-            <p className='m-auto translate-y-[1px] hover:translate-y-[-0.5px]'>
-              {task.done ? 'X' : '✔'}
-            </p>
-          </DoneButton>
-          <Content done={task.done}>
-            <NavLink to={toTask({ _id: task._id })}>{task.content}</NavLink>
-          </Content>
-          <DeleteButton onClick={() => deleteTask(task._id)}>
-            <DeleteIcon className='m-[3px] w-fit h-fit' />
-          </DeleteButton>
-        </li>
-      ))}
+      {exampleTask && !loggedUser && (
+        <>
+          {exampleTasks.map((exampleTask) => (
+            <li
+              className={classNames({
+                'border-b-[1px] border-solid border-alto p-[5px] grid grid-cols-3 items-center gap-[10px] dark:border-gray-400':
+                  true,
+                hidden: exampleTask.done && hidden,
+              })}
+              key={exampleTask._id}
+            >
+              <DoneButton
+                className={classNames({
+                  'w-[25px] h-[25px] border-none cursor:pointer p-0  text-white':
+                    true,
+                  ' bg-limeade hover:bg-japaneseLaurel dark:bg-green-700 ':
+                    exampleTask.done === false,
+                  ' bg-orange-500 hover:bg-orange-600 dark:bg-orange-700 ':
+                    exampleTask.done === true,
+                })}
+                onClick={() => setTaskDone(exampleTask._id)}
+              >
+                <p className='m-auto translate-y-[1px] hover:translate-y-[-0.5px]'>
+                  {exampleTask.done ? 'X' : '✔'}
+                </p>
+              </DoneButton>
+              <Content done={exampleTask.done}>
+                <NavLink to={toTask({ _id: exampleTask._id })}>
+                  {exampleTask.content}
+                </NavLink>
+              </Content>
+              <DeleteButton onClick={() => removeTask(exampleTask._id)}>
+                <DeleteIcon className='m-[3px] w-fit h-fit' />
+              </DeleteButton>
+            </li>
+          ))}
+        </>
+      )}
+      {tasks && loggedUser && (
+        <>
+          {tasks.map((task) => (
+            <li
+              className={classNames({
+                'border-b-[1px] border-solid border-alto p-[5px] grid grid-cols-3 items-center gap-[10px] dark:border-gray-400':
+                  true,
+                hidden: task.done && hidden,
+              })}
+              key={task._id}
+            >
+              <DoneButton
+                className={classNames({
+                  'w-[25px] h-[25px] border-none cursor:pointer p-0  text-white':
+                    true,
+                  ' bg-limeade hover:bg-japaneseLaurel dark:bg-green-700 ':
+                    task.done === false,
+                  ' bg-orange-500 hover:bg-orange-600 dark:bg-orange-700 ':
+                    task.done === true,
+                })}
+                onClick={() => {
+                  task.done === false
+                    ? markTaskAsDone(task._id)
+                    : markTaskAsUndone(task._id);
+                }}
+              >
+                <p className='m-auto translate-y-[1px] hover:translate-y-[-0.5px]'>
+                  {task.done ? 'X' : '✔'}
+                </p>
+              </DoneButton>
+              <Content done={task.done}>
+                <NavLink to={toTask({ _id: task._id })}>{task.content}</NavLink>
+              </Content>
+              <DeleteButton onClick={() => deleteTask(task._id)}>
+                <DeleteIcon className='m-[3px] w-fit h-fit' />
+              </DeleteButton>
+            </li>
+          ))}
+        </>
+      )}
     </ul>
   );
 };
