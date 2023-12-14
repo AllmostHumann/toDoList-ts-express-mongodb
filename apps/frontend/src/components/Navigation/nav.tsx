38f172311aad6a3add144b5690d1d96035a95c87
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-14 23:58:45 +0100

    component update for zustand/RQ coop

--- apps/frontend/src/components/Navigation/nav.tsx
+++ apps/frontend/src/components/Navigation/nav.tsx
@@ -1,14 +1,21 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { toTasks } from '../../routers';
 import { ThemeButton } from '../Buttons/themeButton';
+import { useGetAuthenticadedUser } from '../../api/hooks/users/useGetAuthenticadedUser';
+import { useUserLogout } from '../../api/hooks/users/useLogOut';
 
 interface ModalProps {
-  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowSignupModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowLoginModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const Nav = ({ setShowModal }: ModalProps) => {
+export const Nav = ({ setShowSignupModal, setShowLoginModal }: ModalProps) => {
+  const navigate = useNavigate();
+  const { data: loggedUser } = useGetAuthenticadedUser();
+  const { mutate: logout } = useUserLogout();
+
   return (
-    <nav className='bg-teal dark:bg-sherpaBlue m-0 p-0 flex justify-center items-center list-none'>
+    <nav className='bg-teal dark:bg-sherpaBlue m-0 p-0 flex justify-start items-center list-none md:justify-center'>
       <div className='m-[15px] bg-teal dark:bg-sherpaBlue'>
         <NavLink
           className={({ isActive }) =>
@@ -21,14 +28,34 @@ export const Nav = ({ setShowModal }: ModalProps) => {
           Tasks
         </NavLink>
       </div>
-      <div className='absolute flex justify-center items-center right-0 m-[15px] bg-teal dark:bg-sherpaBlue text-white gap-[20px]'>
+      <div className='absolute flex justify-center items-center right-0 m-[15px] bg-teal dark:bg-sherpaBlue text-white md:gap-[20px] gap-[10px] '>
+        {!loggedUser && (
+          <button
+            className='border-none md:my-0 transition-none hover:scale-100
+          md:mx-[10px] p-0 m-[7px] hover:underline hover:cursor-pointer bg-teal dark:bg-sherpaBlue text-white'
+            onClick={() => setShowSignupModal(true)}
+          >
+            Sign up
+          </button>
+        )}
         <button
           className='border-none md:my-0 transition-none hover:scale-100
           md:mx-[10px] p-0 m-[7px] hover:underline hover:cursor-pointer bg-teal dark:bg-sherpaBlue text-white'
-          onClick={() => setShowModal(true)}
+          onClick={() => setShowLoginModal(true)}
         >
-          Log in
+          {loggedUser ? `Signed in as: ${loggedUser.username}` : 'Log in'}
         </button>
+        {loggedUser && (
+          <button
+            className='border-none md:my-0 transition-none hover:scale-100
+          md:mx-[10px] p-0 m-[7px] hover:underline hover:cursor-pointer bg-teal dark:bg-sherpaBlue text-white'
+            onClick={() => {
+              logout(), navigate(toTasks());
+            }}
+          >
+            Log out
+          </button>
+        )}
         <ThemeButton />
       </div>
     </nav>
