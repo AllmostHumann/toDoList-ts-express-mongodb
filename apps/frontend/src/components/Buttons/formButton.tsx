233f062c3fdad630c7cc72010304913bfe17d572
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-14 23:58:00 +0100

    formButton component update

--- apps/frontend/src/components/Buttons/formButton.tsx
+++ apps/frontend/src/components/Buttons/formButton.tsx
@@ -1,7 +1,8 @@
-export const FormButton = () => {
-  return (
-    <button className='border-none cursor-pointer p-[10px] w-[100%] md:hover:scale-[1.15] hover:scale-[1.03] dark:bg-sherpaBlue bg-teal text-white'>
-      Add task
-    </button>
-  );
+interface ButtonProps {
+  children: React.ReactNode;
+  className: string;
+}
+
+export const FormButton = ({ children, className }: ButtonProps) => {
+  return <button className={className}>{children}</button>;
 };
