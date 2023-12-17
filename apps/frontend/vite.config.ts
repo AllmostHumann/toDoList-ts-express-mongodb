f9b0babc1407214acf9e399e4e496539959ceaf7
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-17 12:43:04 +0100

    test

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,5 +5,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ include: '**/*.svg?react' })],
+  // base: '/todolist-ts-express-mongodb/apps/frontend/',
   base: '/todolist-ts-express-mongodb/apps/frontend/',
 });
