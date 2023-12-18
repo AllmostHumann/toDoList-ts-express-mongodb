353d8384abf28dabd2843bc9cb9a87827da1c3e3
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-18 06:59:00 +0000

    Update getExampleTasks.ts

--- apps/frontend/src/utils/getExampleTasks.ts
+++ apps/frontend/src/utils/getExampleTasks.ts
@@ -3,7 +3,7 @@ import { Tasks } from '../api/types/task';
 
 export const getExampleTasks = async () => {
   const response = await axios.get<Tasks>(
-    'https://todolist-ts-mern-frontend-allmosthumann.vercel.app//exampleTasks.json',
+    'https://todolist-ts-mern-frontend.onrender.com/exampleTasks.json',
   );
   return response.data;
 };
